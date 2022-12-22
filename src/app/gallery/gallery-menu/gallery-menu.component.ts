0ac97d2315d316cbd73646238908755da25611be
josh <jyw2@ualberta.ca>
2022-12-22 15:05:17 -0700

    Fix Default api call

--- src/app/gallery/gallery-menu/gallery-menu.component.ts
+++ src/app/gallery/gallery-menu/gallery-menu.component.ts
@@ -111,7 +111,7 @@ export class GalleryMenuComponent implements OnInit {
 
   populateImages() {
     //used on first page load to load all images
-    this.httpClient.get('https://jyuenwapi.azurewebsites.net').subscribe((images) => {
+    this.httpClient.get('https://jyuenwapi.azurewebsites.net/all').subscribe((images) => {
       //send Images to the main gallery component to render
       this.apiCall.emit(images)
     })
